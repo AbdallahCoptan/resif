65e65c869a7d57f50e3c73506680cdcfa05afeb7
Sarah Diehl <sarah.diehl@uni.lu>
2017-03-03 13:08:20 +0100

    Lock doesn't work in main function. Get's release before execution of actual command.

--- resif/resif.py
+++ resif/resif.py
@@ -7,8 +7,6 @@
 import click
 import subprocess
 import pkg_resources
-import fcntl
-import os
 
 from .cli.init import init
 from .cli.build import build
@@ -32,14 +30,6 @@ def resif(ctx, version):
 
     Choose the sub-command you want to execute.
     """
-
-    pid_file = os.path.join(os.path.expanduser("~"),'resif.lock')
-    fp = open(pid_file, 'w')
-    try:
-        fcntl.lockf(fp, fcntl.LOCK_EX | fcntl.LOCK_NB)
-    except IOError:
-        # another instance is running
-        exit("Another instance of RESIF is already running. Only one instance is allowed to run at a time.")
 
     if ctx.invoked_subcommand is None:
         if version:
