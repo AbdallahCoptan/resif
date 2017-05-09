1b428026bce1a56f172182e907f02660beae9ee5
Sarah Diehl <sarah.diehl@uni.lu>
2017-05-09 10:20:34 +0200

    Search one level higher up for ebfile, since figuring out the sw name is error prone.

--- resif/utilities/swset.py
+++ resif/utilities/swset.py
@@ -28,15 +28,13 @@ def getSoftwareSets (resifile):
 
 # Check if an easyconfig file exists
 def checkEasyConfig (ebfile, ebpaths):
-    found = False
-    match = re.match("^([\w-]+)-(v?[\d\.-]+)-(\w+)-?([\d\w.]+)?(.*)\.eb", ebfile)
-    swname = match.group(1)
 
     for path in ebpaths:
-        p = os.path.join(path, swname[0].lower(), swname)
-        if os.path.isdir(p) and ebfile in os.listdir(p):
-            found = True
-    return found
+        p = os.path.join(path, ebfile[0].lower())
+        for root, dirs, files in os.walk(p):
+            if ebfile in files:
+                return True
+    return False
 
 # Look for similar easyconfigs (same toolchain, same year)
 def findSimilarEasyConfig (ebfile, ebpaths):
